f4da8f046a6b9ee3f5efbff625fe17cfeb4246e6
Opsis-CX <becky@turritopsisconsulting.com>
2026-07-12 22:49:34 -0400

    Add Scorecard route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ import Calendar from './modules/Calendar'
 import WeeklySync from './modules/WeeklySync'
 import Notifications from './modules/Notifications'
 import KnowledgeBase from './modules/KnowledgeBase'
+import Scorecard from './modules/Scorecard'
 import { UnreadProvider } from './lib/unread'
 // --- hiring pipeline ---
 import ApplicationForm from './modules/ApplicationForm'
@@ -113,6 +114,7 @@ function AuthedApp({ session, isAdmin, appRole, navOpen, setNavOpen, location })
               <Route path="/settings" element={<Settings />} />
               <Route path="/notifications" element={<Notifications />} />
               <Route path="/knowledge" element={<KnowledgeBase />} />
+              {canAny(appRole, 'service_performance_scorecard') && <Route path="/scorecard" element={<Scorecard />} />}
               <Route path="/schedule" element={<Schedule />} />
               <Route path="/chat" element={canAny(appRole, 'chat') ? <Chat /> : <Placeholder title="No access" note="You don't have access to this area." />} />
               {canAny(appRole, 'certifications.all') && <Route path="/certifications" element={<Certifications />} />}
