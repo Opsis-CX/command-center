89d1ffc7e15e5ae3974e3c0dc4a7ade6f6f9ef07
Opsis-CX <becky@turritopsisconsulting.com>
2026-08-12 21:06:33 -0400

    Add Clarity tracking script to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "y1hoag1aii");
+</script>
+
+
+
 import NotificationBell from './components/NotificationBell'
 import HeaderTaskBar from './components/HeaderTaskBar'
 import { useState, useEffect } from 'react'
